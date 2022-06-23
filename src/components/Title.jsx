bb4eae617b16a3f2f89870a425308a38b45949d1
Linhx <phantranthelinh@gmail.com>
2022-06-24 00:11:36 +0700

    up

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Title = () => {
   return (
     <div className="title">
-      <h1>Photos Gallery</h1>
+      <h1>Photo Gallery</h1>
       <h2>Upload Your Pictures</h2>
       <p>Choose a picture that you like the most</p>
     </div>
